bd4c5b995109762857c5ebc4a93796de4ac8ca16
Wajahat-Rana <wajahatsaleem59@gmail.com>
2024-07-30 16:55:28 +0500

    Adding Movement to Player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float playerMovementSpeed = 4f;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 temp = transform.position;
+        temp.x += playerMovementSpeed * Time.deltaTime;
+
+        transform.position = temp;
+    }
+}
