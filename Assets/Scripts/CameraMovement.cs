e7a36556e071a36276f38561c2b0766837e078a7
Wajahat-Rana <wajahatsaleem59@gmail.com>
2024-07-30 16:46:31 +0500

    Adding Movement to Camera

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private float cameraMovementSpeed = 4f;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 temp = transform.position;
+        temp.x += cameraMovementSpeed * Time.deltaTime;
+
+        transform.position = temp;
     }
 }
