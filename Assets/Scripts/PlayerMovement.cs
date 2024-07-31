8ffcc4dc38193f903354eedeca4ba512e265441c
Wajahat-Rana <wajahatsaleem59@gmail.com>
2024-07-31 13:24:04 +0500

    Added Gravity Shifter

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    private float playerMovementSpeed = 3f;
+    private Button shifter;
     private Rigidbody2D rb;
-    private float playerMovementSpeed = 4f;
-    // Start is called before the first frame update
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        shifter = GameObject.Find("Shifter").GetComponent<Button>();
+        shifter.onClick.AddListener(shiftGravity);
     }
 
     // Update is called once per frame
@@ -18,5 +25,12 @@ public class PlayerMovement : MonoBehaviour
         Vector3 temp = transform.position;
         temp.x += playerMovementSpeed * Time.deltaTime;
         transform.position = temp;
+    }
+    void shiftGravity()
+    {
+        rb.gravityScale *= -1;
+        Vector3 temp = rb.transform.localScale;
+        temp.y *= -1;
+        rb.transform.localScale = temp;
     }
 }
