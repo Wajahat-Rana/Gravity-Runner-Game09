442012561343f8a1df8b592cf262d125a37dcd0d
Wajahat-Rana <wajahatsaleem59@gmail.com>
2024-08-13 14:08:59 +0500

    Detecting Rockets And Coins & Score Count + Game Restart Functionality

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 
 public class PlayerMovement : MonoBehaviour
@@ -11,12 +12,15 @@ public class PlayerMovement : MonoBehaviour
     private float playerMovementSpeed = 3f;
     private Button shifter;
     private Rigidbody2D rb;
+    private TextMeshProUGUI score;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         shifter = GameObject.Find("Shifter").GetComponent<Button>();
         shifter.onClick.AddListener(shiftGravity);
+        score = GameObject.Find("Score").GetComponent<TextMeshProUGUI>();
+        score.text = "0";
     }
 
     // Update is called once per frame
@@ -32,5 +36,19 @@ public class PlayerMovement : MonoBehaviour
         Vector3 temp = rb.transform.localScale;
         temp.y *= -1;
         rb.transform.localScale = temp;
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Rocket"){
+            StartCoroutine(KillPlayerAndRestartGame());
+        }
+        if(other.tag == "Coin"){
+            score.text = (int.Parse(score.text) + 1).ToString();
+        }
+    }
+     IEnumerator KillPlayerAndRestartGame(){
+        transform.position = new Vector3(200,200,0);
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene( SceneManager.GetActiveScene().buildIndex);
     }
 }
